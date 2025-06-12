a38498f84efc07722abcc37bf29d2fc7e71debf8
Jacob Ochoa <jacob.ochoa@gmail.com>
2025-06-11 18:56:00 -0700

    Rename methods. Update examples. Fix tests. (#3)
    This renames some exported methods of the mutex, adds examples, and adds
    more testing.
    
    The readme is updated with examples as well.

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package mutex_test
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jakobii/mutex"
 )
@@ -22,17 +23,17 @@ func ExampleMutex() {
 		mu.Unlock()
 	}
 
-	// WaitLock
+	// SendLock
 	select {
-	case mu.WaitLock() <- struct{}{}:
+	case mu.SendLock() <- struct{}{}:
 		fmt.Println("This mutex plays nice with select statements.")
 		mu.Unlock()
 	default:
 		return
 	}
 
-	// LockCtx
-	if err := mu.LockCtx(context.Background()); err != nil {
+	// GetLock
+	if err := mu.GetLock(context.Background()); err != nil {
 		return
 	}
 	fmt.Println("Which means it can easily respect contexts.")
@@ -43,4 +44,46 @@ func ExampleMutex() {
 	// Why not give it a try?
 	// This mutex plays nice with select statements.
 	// Which means it can easily respect contexts.
+}
+
+func ExampleMutex_GetLock() {
+	var mu mutex.Mutex
+
+	// Imagine some other process has the lock.
+	mu.Lock()
+
+	// Attempt to acquire the lock with a context. This timeout is short for the
+	// sake of the completion of the example. In practice this might be a larger
+	// duration for detecting deadlocks, or a context tied to some other process
+	// cancelation.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
+
+	if err := mu.GetLock(ctx); err != nil {
+		fmt.Println("Failed to acquire lock:", err)
+		return
+	}
+	defer mu.Unlock()
+
+	// Output:
+	// Failed to acquire lock: context deadline exceeded
+}
+
+func ExampleMutex_SendLock() {
+	var mu mutex.Mutex
+
+	// The process that sends on the mutex channel is the one that obtains the
+	// lock.
+	select {
+	case <-time.After(time.Millisecond):
+		fmt.Println("Failed to acquire lock: timeout")
+		return
+	case mu.SendLock() <- struct{}{}:
+		defer mu.Unlock()
+		// ..do things while holding the lock...
+		fmt.Println("Successfully acquired lock.")
+	}
+
+	// Output:
+	// Successfully acquired lock.
 }
--- mutex.go
+++ mutex.go
@@ -5,18 +5,64 @@ import (
 	"sync"
 )
 
-// Mutex is a drop-in replacement for the standard libraries sync.Mutex. It
-// offers the ability cancel waiting to obtain a lock by making use of Go's
-// select statement.
-//
-// The zero value is safe to use.
+// Mutex is a drop-in replacement for the standard library's sync.Mutex. It
+// offers the ability to cancel acquiring a lock by making use of Go's select
+// statement. The zero value is safe to use. Satisfies sync.Locker.
 type Mutex struct {
+	// initMu is used to ensure that the mutex is initialized only once.
 	initMu sync.Mutex
 
-	// state must be a buffered mmutex of 1.
-	// locked: len(state) == 1
-	// unlocked: len(state) == 0
+	// state must be a buffered channel with a length of 1. It is considered
+	// locked when it has a length of 1. It is considered unlocked when it has a
+	// length of 0.
 	state chan struct{}
+}
+
+// Unlocks m. Panics if m is not locked. Calling Unlock on an unlocked mutex
+// usually indicates a race condition.
+func (m *Mutex) Unlock() {
+	m.init()
+	select {
+	case <-m.state:
+	default:
+		panic("unlock of unlocked mutex")
+	}
+}
+
+// SendLock locks m when sending to its returned channel. Do not close it.
+// Calling SendLock while holding the lock will deadlock.
+func (m *Mutex) SendLock() chan<- struct{} {
+	m.init()
+	return m.state
+}
+
+// Lock locks m. If the lock is already in use, the calling goroutine blocks
+// until the mutex is available.
+func (m *Mutex) Lock() {
+	m.SendLock() <- struct{}{}
+}
+
+// TryLock tries to lock m and reports whether it succeeded.
+//
+// Note that while correct uses of TryLock do exist, they are rare, and use of
+// TryLock is often a sign of a deeper problem in a particular use of mutexes.
+func (m *Mutex) TryLock() bool {
+	select {
+	case m.SendLock() <- struct{}{}:
+		return true
+	default:
+		return false
+	}
+}
+
+// GetLock locks m or returns ctx's error.
+func (m *Mutex) GetLock(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case m.SendLock() <- struct{}{}:
+		return nil
+	}
 }
 
 // init is used to fix a zero value Mutex.
@@ -28,53 +74,9 @@ func (m *Mutex) init() {
 	}
 }
 
-// WaitLock give access to internal state of the mutex and a send chan. do not
-// close the chan, it will be cleaned up by gc. closing the channel will break
-// the Mutex. Only use the chan it to send en empty struct to obtain the lock.
-func (m *Mutex) WaitLock() chan<- struct{} {
-	m.init()
-	return m.state
-}
-
-// LockCtx is a convience func for selecting WaitLock and ctx. If the ctx is
-// done the ctx error will be returned.
-func (m *Mutex) LockCtx(ctx context.Context) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case m.WaitLock() <- struct{}{}:
-		return nil
-	}
-}
-
-// Lock locks m. If the lock is already in use, the calling goroutine blocks
-// until the mutex is available.
-func (m *Mutex) Lock() {
-	m.WaitLock() <- struct{}{}
-}
-
-// TryLock tries to lock m and reports whether it succeeded.
-//
-// Note that while correct uses of TryLock do exist, they are rare, and use of
-// TryLock is often a sign of a deeper problem in a particular use of mutexes.
-func (m *Mutex) TryLock() bool {
-	select {
-	case m.WaitLock() <- struct{}{}:
-		return true
-	default:
-		return false
-	}
-}
-
-// Unlock unlocks m. It is a run-time error if m is not locked on entry to
-// Unlock.
-//
-// Calling unlock on an unlocked lock is generally an indication of a race
-// condition.
-func (m *Mutex) Unlock() {
-	select {
-	case <-m.state:
-	default:
-		panic("unlock of unlocked mutex")
-	}
+// IsLocked is mostly used for testing purposes. While the method is safe for
+// concurrent use, the returned value should no be used as a synchonization
+// mechanism as m's state may change before the returned value is evaluated.
+func (m *Mutex) IsLocked() bool {
+	return len(m.state) == 1
 }
